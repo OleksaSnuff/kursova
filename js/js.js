c471f1634a83dc45aa87101b6fe34bc2b8300d65
OleksaSnuff <alex1204park@gmail.com>
2023-10-17 12:28:47 +0300

    upd

---
+++ js/js.js
@@ -0,0 +1,6 @@
+/*document.getElementById("zero-butt").addEventListener("click", function () {
+  document.getElementById("first-section").classList.add("visually-hidden");
+});*/
+document.getElementById("zero-butt").addEventListener("click", function () {
+  document.getElementById("first-section").classList.remove("visually-hidden");
+});
